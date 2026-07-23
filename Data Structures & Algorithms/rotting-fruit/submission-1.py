5716fc7c5951b50a1829ea2057417eb8e75dd3f8
Richard Tan <richardtan787@gmail.com>
2026-07-23 00:17:14 -0400

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+        visited = set()
+        rows = len(grid)
+        cols = len(grid[0])
+        healthyFruit = 0
+        infectedFruit = 0
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        q = deque()
+        maxTime = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    healthyFruit += 1
+                elif grid[r][c] == 2:
+                    q.append((r, c))
+        if healthyFruit == 0:
+            return 0
+        while q and infectedFruit < healthyFruit:
+            l = len(q)
+            maxTime += 1
+            for _ in range(l):
+                cr, cc = q.popleft()
+                print(q)
+                for dr, dc in dirs:
+                    nr, nc = cr + dr, cc + dc
+                    
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1 and (nr, nc) not in visited:
+                        print(nr, nc, maxTime)
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+                        grid[nr][nc] = 2
+                        infectedFruit += 1
+                        
+            
+        return maxTime if infectedFruit == healthyFruit else -1
+                        
+                
